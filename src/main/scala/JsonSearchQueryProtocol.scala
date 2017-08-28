b4e8c1500600e245c90d2d9c86e5f683d5bf7c60
Aleksey Lisikh <lizard5472@gmail.com>
2017-08-28 20:24:39 +0200

    improved json parser, but still not done yet

--- src/main/scala/JsonSearchQueryProtocol.scala
+++ src/main/scala/JsonSearchQueryProtocol.scala
@@ -17,16 +17,45 @@ trait JsonSearchQueryProtocol extends DefaultJsonProtocol {
 
   implicit object AndBlockJsonFormat extends RootJsonFormat[AndBlock] {
     def read(value: JsValue) = {
+      // TODO: improve this code.
       val expressions = value.asJsObject.fields.map { case (variable, exprValue) =>
         exprValue match {
           case JsString(jsString) =>
-            SimpleExpression(variable, jsString)
+            SimpleExpr(variable, jsString)
           case JsNumber(jsNumber) =>
-            SimpleExpression(variable, jsNumber)
+            SimpleExpr(variable, jsNumber)
           case JsBoolean(jsBool) =>
-            SimpleExpression(variable, jsBool)
+            SimpleExpr(variable, jsBool)
           case JsObject(expr) =>
-            ComplexExpression(variable, expr)
+            val exprValues: List[ExprValue] = expr.map { case (operation, exprValue) =>
+                if (operation == "in") {
+                  exprValue match {
+                    case JsArray(elements) =>
+                      InExpr(elements.toList)
+                    case _ =>
+                      throw new UnsupportedOperationException("'in' operation supports only array-typed value")
+                  }
+                } else if (operation == "ne") {
+                  exprValue match {
+                    case JsString(jsString) => NeExpr(jsString)
+                    case JsNumber(jsNumber) => NeExpr(jsNumber)
+                    case _ =>
+                      throw new UnsupportedOperationException("'ne' operation supports only number and string value")
+                  }
+                } else if (operation == "gte") {
+                  GteExpr(exprValue)
+                } else if (operation == "lte") {
+                  LteExpr(exprValue)
+                } else if (operation == "lt") {
+                  LtExpr(exprValue)
+                } else if (operation == "gt") {
+                  GtExpr(exprValue)
+                } else {
+                  throw new UnsupportedOperationException(s"operation $operation is not supported")
+                }
+            }.toList
+
+            ComplexExpr(variable, exprValues)
           case _ =>
             throw new UnsupportedOperationException("not supported.")
         }
@@ -35,7 +64,7 @@ trait JsonSearchQueryProtocol extends DefaultJsonProtocol {
       AndBlock(expressions)
     }
 
-    def write(andBlock: AndBlock) = ???
+    def write(andBlock: AndBlock) = throw new UnsupportedOperationException("conversion to json is not supported")
   }
 
   def sampleJson =
@@ -68,10 +97,17 @@ trait JsonSearchQueryProtocol extends DefaultJsonProtocol {
 
   case class SearchQuery(query: Query, count: Option[Int] = Some(50), offset: Option[Int] = Some(0))
   case class Query(and: AndBlock)
-  case class AndBlock(expressions: List[Expression])
+  case class AndBlock(expressions: List[Expr])
 
-  abstract class Expression(variable: String, value: Any)
-  case class ComplexExpression(variable: String, expression: Map[String, JsValue]) extends Expression(variable, expression)
-  case class SimpleExpression(variable: String, value: Any) extends Expression(variable, value)
+  abstract class Expr(variable: String, value: Any)
+  case class ComplexExpr(variable: String, expression: List[ExprValue]) extends Expr(variable, expression)
+  case class SimpleExpr(variable: String, value: Any) extends Expr(variable, value)
 
+  abstract class ExprValue(operation: String, value: Any)
+  case class InExpr(value: List[Any]) extends ExprValue("in", value)
+  case class NeExpr(value: Any) extends ExprValue("ne", value)
+  case class GteExpr(value: Any) extends ExprValue("gte", value)
+  case class LteExpr(value: Any) extends ExprValue("lte", value)
+  case class LtExpr(value: Any) extends ExprValue("lt", value)
+  case class GtExpr(value: Any) extends ExprValue("gt", value)
 }
